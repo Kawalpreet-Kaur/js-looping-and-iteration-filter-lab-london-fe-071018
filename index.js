13b6787797ad2d360b1a6d888af6869260306085
Kawalpreet Kaur <kawalpreet.kaur@hotmail.com>
2018-08-19 19:41:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Code your solution in this file
+function findMatching(drivers,matchString){
+  let matchedString=drivers.filter((matchedValue,matchString)=>
+  matchedValue.toLowerCase()===matchString.toLowerCase());
+  return matchedString ;
+}
