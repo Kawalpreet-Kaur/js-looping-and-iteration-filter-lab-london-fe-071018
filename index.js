ec1f72337d27cf7349448b538bffd771baac214c
Kawalpreet Kaur <kawalpreet.kaur@hotmail.com>
2018-08-19 19:47:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 // Code your solution in this file
+// function findMatching(drivers,matchString){
+//   let matchedString=drivers.filter((matchedValue,matchString)=>
+//   matchedValue.toLowerCase()===matchString.toLowerCase());
+//   return matchedString ;
+// }
+
 function findMatching(drivers,matchString){
   let matchedString=drivers.filter((matchedValue,matchString)=>
-  matchedValue.toLowerCase()===matchString.toLowerCase());
+  matchedValue===matchString;
   return matchedString ;
 }
